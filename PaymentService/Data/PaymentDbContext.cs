7554d2aae8b5411a5928de4d9c406689168351c5
YuriKunyk <kunyk.yuri@gmail.com>
2025-10-28 19:34:41 +0200

    feature: added configuration for payment entity implemented via IEntityTypeConfiguration interface

--- PaymentService/Data/PaymentDbContext.cs
+++ PaymentService/Data/PaymentDbContext.cs
@@ -14,39 +14,6 @@ public class PaymentDbContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Payment>(entity =>
-        {
-            //Primary Key
-            entity.HasKey(e => e.Id);
-            
-            // Column configurations
-            entity.Property(e => e.IdempotencyKey)
-                .IsRequired()
-                .HasMaxLength(255);
-            
-            entity.Property(e => e.Amount)
-                .HasPrecision(19, 4);
-            
-            entity.Property(e => e.Currency)
-                .IsRequired()
-                .HasMaxLength(3);
-            
-            entity.Property(e => e.ProviderPaymentId)
-                .HasMaxLength(255);
-            
-            // Enum to int conversion
-            entity.Property(e => e.Status)
-                .HasConversion<int>();
-            
-            // Timestamps
-            entity.Property(e => e.CreatedAt)
-                .IsRequired();
-            
-            entity.Property(e => e.UpdatedAt)
-                .IsRequired();
-
-        });
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PaymentDbContext).Assembly);
     }
 }
